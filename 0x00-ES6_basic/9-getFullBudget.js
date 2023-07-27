33d6e6188e88ac610423d4304922d822287d673e
Abraham Grade <gradeisageek@gmail.com>
2023-07-27 02:01:12 +0000

    solution to task 9 added

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,12 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars : (income) => {return `$${income}`;},
-    getIncomeInEuros : (income) => {return `${income} Euros`;},
-    ...budget
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} Euros`,
+    ...budget,
   };
 
   return fullBudget;
 }
-
-
-

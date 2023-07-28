1dc6b334be337a7e4e7df461136918c09c1c6983
cgrade <gradeisageek@gmail.com>
2023-07-28 01:20:51 +0100

    correction to 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,10 +1,5 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
+    this.maxStudentsSize = maxStudentsSize;
   }
-};
-
-
-const room = new ClassRoom(10);
-
-console.log(room._maxStudentsSize);
+}

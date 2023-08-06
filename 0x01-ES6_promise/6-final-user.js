16fbaceae4f6b071bbf4b8451bfaf34e4c604316
cgrade <gradeisageek@gmail.com>
2023-08-06 13:28:38 +0100

    correction to task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]).then((res) => { console.log(res); });
+  return Promise.allSettled([user, photo]).then((res) => { 
+    const newArry = [];
+    for (let i of res) {
+      if (i.status === 'fulfilled') {
+        newArry.push(i)
+        } else {
+          i.reason = Error();
+          newArry.push(i)
+        }
+      return newArry;
+    }
+        
+    });
 }

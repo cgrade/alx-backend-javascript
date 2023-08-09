b7db2fb79c9ffb3e475d8391ab7558e690d2ae5a
cgrade <gradeisageek@gmail.com>
2023-08-09 01:46:19 +0100

    solution to task 2 added

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+/* eslint no-underscore-dangle: 0 */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be string');
+    if (!Number.isInteger(length)) throw new TypeError('Lenght must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students must be Array');
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (!Number.isInteger(length)) throw new TypeError('Lenght must be a number');
+    this.length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set studnets(students) {
+    if (!Array.isArray(students)) throw new TypeError('students must be Array');
+    this._students = students;
+  }
+}

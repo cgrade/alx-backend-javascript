6497b68869d6da153b1f65a4cb894cc704367a37
cgrade <gradeisageek@gmail.com>
2023-08-09 01:17:12 +0100

    solution updated

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(29),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const class1 = new ClassRoom(29);
+  const class2 =  new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return newList;
 }

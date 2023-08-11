2f1f76bf10ebaa12125a6c1b221b00d768bc9dd3
cgrade <gradeisageek@gmail.com>
2023-08-11 10:26:23 +0100

    solution updated

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,8 +1,7 @@
-let car = Symbol();
-
+/* eslint no-underscore-dangle: 0 */
 
 export default class Car {
-  constructor(brand, motor, color){
+  constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;

17837169d946d0d1fe303a6cdd3df574a7da75e3
cgrade <gradeisageek@gmail.com>
2023-09-24 00:24:00 +0100

    solution to task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const { parse } = require('csv-parse');
+
+const sweArr = [];
+
+function countStudents(path) {
+  fs.createReadStream(path)
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on('error', () => {
+      throw new Error('Cannot load the databse');
+    })
+    .on('data', (row) => {
+      sweArr.push(row);
+    })
+    .on('end', () => {
+      const numOfStudent = sweArr.length;
+      const csStudents = [];
+      const sweStudents = [];
+      const listOfFirstNamesinCS = [];
+      const listOfFirstNamesinswe = [];
+
+      for (const student of sweArr) {
+        if (student[3] === 'CS') {
+          csStudents.push(student);
+        } else {
+          sweStudents.push(student);
+        }
+      }
+      csStudents.forEach((student) => {
+        listOfFirstNamesinCS.push(student[0]);
+      });
+
+      sweStudents.forEach((student) => {
+        listOfFirstNamesinswe.push(student[0]);
+      });
+
+      console.log(`Number of students: ${numOfStudent}`);
+      console.log(`Number of students in CS: ${csStudents.length} List: ${listOfFirstNamesinCS}`);
+      console.log(`Number of students in SWE: ${sweStudents.length} List: ${listOfFirstNamesinswe}`);
+    });
+}
+
+module.exports = countStudents;

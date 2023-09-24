eb82218e761f42bcaa3d59cdf8cefb55a8931f14
cgrade <gradeisageek@gmail.com>
2023-09-24 03:23:41 +0100

    correction to project 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,25 +1,28 @@
 const express = require('express');
-
-const countStudents = require('./3-read_file_async');
-
-const child = require('child_process');
+const readDatabase = require('./full_server/utils.js');
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
-  res.status = 200;
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  countStudents(process.argv[2].toString()).then((output) => {
-    res.send(['This is the list of our students', output].join('\n'));
-  }).catch(() => {
-    res.send('This is hte list of our students\n Cannot load the database');
-  });
+app.get('/students', async (req, res) => {
+  const databasePath = process.argv[2];
+
+  try {
+    const students = await readDatabase(databasePath);
+
+    const studentsList = Object.values(students).map(students => students.join(', ')).join('\n');
+
+    res.send(`This is the list of our students:\n${studentsList}`);
+  } catch (error) {
+    res.status(500).send('Error reading database');
+  }
 });
 
-app.listen(port);
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
 
 module.exports = app;
